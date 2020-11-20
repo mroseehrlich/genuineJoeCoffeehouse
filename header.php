d6c01135af2b317b0ef88d8bea0c0680362374f8
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2020-11-20 15:35:37 -0600

    added navbar-nav to nav list

--- header.php
+++ header.php
@@ -138,29 +138,27 @@
 		    </a>
 
 			</div>
-			<div id="main_navbar" class="navbar navbar-expand-md navbar-light bg-light">
+			<nav id="main_navbar" class="navbar navbar-expand-md navbar-inverse">
     <!-- you can remove this container wrapper if you want things full width -->
-    <div class="container">
-        <a class="navbar-brand d-none" href="#"><?php esc_html_e( bloginfo( 'name' ), 'themeslug' ); ?></a>
-
+		<!-- <a class="navbar-brand d-none" href="#"><?php esc_html_e( bloginfo( 'name' ), 'themeslug' ); ?></a> -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#headerNav" aria-controls="headerNav" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'best-reloaded' ); ?>">
             <span class="navbar-toggler-icon"></span><span class="sr-only"><?php esc_html_e( 'Toggle Navigation', 'themeslug' ); ?></span>
         </button>
-        <nav class="collapse navbar-collapse" id="headerNav" role="navigation" aria-label="Main Menu">
+        <div class="collapse navbar-collapse" id="headerNav" role="navigation" aria-label="Main Menu">
             <span class="sr-only"><?php esc_html_e( 'Main Menu', 'themeslug' ); ?></span>
             <?php
             wp_nav_menu( array(
                 'theme_location' => 'primary',
                 'depth' => 2,
                 'container' => false,
-                'menu_class' => 'nav',
+                'menu_class' => 'nav navbar-nav',
                 'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
                 'walker' => new WP_Bootstrap_Navwalker(),
             ) );
         ?>
-        </nav>
-    </div>
-</div>
+        </div>
+		</nav>	
+
 		</header>
 		<div class="wrapper container-fluid">
 
