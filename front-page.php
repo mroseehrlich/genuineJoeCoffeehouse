a8a0ecea6c0f9b99207635afa3a61597cb61f670
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2020-11-21 16:56:53 -0600

    add jumbotron

--- front-page.php
+++ front-page.php
@@ -8,6 +8,12 @@
  */
  get_header(); ?>
 
+<main>
+    <div class="jumbotron jumbotron-fluid">
+        <h1>Genuine Joe Coffeehouse</h1>
+        <p class=lead>Stay kind. Stay genuine.</p>
+    </div>
+</main>
 
 
  <?php get_footer() ?>
---
+++ page-menu.php
@@ -0,0 +1,361 @@
+<?php
+/**
+ * Template Name: Menu Page
+ * 
+ * @package WordPress
+ * @subpackage HTML5-Reset-WordPress-Theme
+ * @since HTML5 Reset 2.0
+ */
+ get_header(); ?>
+
+<div class="row">
+    <div class="col-md-6">
+        <div class="menu_hot-drinks">
+            <h1 class="menu_type">Hot Coffee and Tea</h1>
+            <ul class="menu_content">
+ 	            <li class="product">
+                    <h3 class="product_title">Drip Coffee</h3>
+                    <div class="product_price">
+                        <ul class="product_variations">
+                            <li><span class="size">12oz</span> <span class="price">$2.25</span></li>
+                            <li><span class="size">20oz</span> <span class="price">$2.75</span></li>
+                            <li><span class="size">Bottomless Refills </span><span class="price">$3.75</span></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="product">
+                    <h3 class="product_title">Cafe Au Lait</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span> <span class="price">$3.00</span></li>
+                            <li><span class="size">20oz</span> <span class="price">$3.50</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div></li>
+ 	            <li class="product">
+                    <h3 class="product_title">Hot Tea</h3>
+                    <div class="product_price">
+                    <ul>
+                        <li>
+                            <span class="size">20oz</span>
+                            <span class="price">$2.75</span>
+                        </li>
+                    </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Tea Fog</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">20oz</span> <span class="price">$3.50</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>Hot tea, steamed milk, a touch of your choice of Monin syrup, topped with a touch of milk foam.</p>
+                    </div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Chai Latte</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span> <span class="price">$3.50</span></li>
+                            <li><span class="size">20oz</span> <span class="price">$5.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Dirty chai</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span> <span class="price">$5.00</span></li>
+                            <li><span class="size">20oz</span> <span class="price">$5.75</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Matcha Latte</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span> <span class="price">$4.25</span></li>
+                            <li><span class="size">20oz</span> <span class="price">$5.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Hot Chocolate</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span> <span class="price">$3.25</span></li>
+                            <li><span class="size">20oz</span> <span class="price">$4.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Steamer</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span> <span class="price">$3.25</span></li>
+                            <li><span class="size">20oz</span> <span class="price">$4.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>
+                            Steamed milk with your choice of Monin simple syrup. Try one with Vanilla and Honey!
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div class="menu-iced-drinks">
+            <h1 class="menu_type">Iced Coffee and Tea</h1>
+            <ul class="menu_content">
+                <li class="product">
+                    <h3 class="product_title">Cold Brew</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span><span class="price">$3.25</span></li>
+                            <li><span class="size">20oz</span><span class="price">$4.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+                <li class="product">
+                    <h3 class="product_title">Iced Tea</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">20oz</span><span class="price">$2.75</span></li>
+                            <li><span class="size">Bottomless Refills</span><span class="price">$3.75</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>Choose between Black, Green, or Hibiscus</p>
+                    </div>
+                </li>
+                <li class="product">
+                    <h3 class="product_title">Italian Soda</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">20oz</span><span class="price">$3.25</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">Made with Topo Chico and Monin cane sugar simple syrups. Ask about our
+                    flavor options and feel free to mix and match!</div>
+                </li>
+                <li class="product">
+                    <h3 class="product_title">Iced Chai</h3>
+                    <div class="product_price">
+                    <ul>
+                        <li><span class="size">12oz</span><span class="price">$4.25</span></li>
+                        <li><span class="size">20oz</span><span class="price">$5.00</span></li>
+                    </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+                <li class="product">
+                    <h3 class="product_title">Iced Dirty Chai</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span><span class="price">$5.00</span></li>
+                            <li><span class="size">20oz</span><span class="price">$5.75</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+                <li class="product">
+                    <h3 class="product_title">Iced Matcha Latte</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span><span class="price">$4.25</span></li>
+                            <li><span class="size">20oz</span><span class="price">$5.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+                <li class="product">
+                    <h3 class="product_title">Cold Brew Frappe</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">20oz</span><span class="price">$5.25</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>Choose between Mocha, Vanilla, or Horchata</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-6">
+        <div class="menu-espresso-drinks">
+            <h1 class="menu_type">Traditional Espresso Drinks</h1>
+            <ul class="menu_content">
+                <li class="product">
+                    <h3 class="product_title">Espresso</h3>
+                    <div class="product_price">
+                    <ul>
+                        <li><span class="size">Single</span><span class="price">$2.25</span></li>
+                        <li><span class="size">Double</span><span class="price">$2.75</span></li>
+                    </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+                <li class="product">
+                    <h3 class="product_title">Macchiato</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">Single</span><span class="price">$2.50</span></li>
+                            <li><span class="size">Double</span><span class="price">$3.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+                <li class="product">
+                    <h3 class="product_title">Cortado</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">Single</span><span class="price">$2.50</span></li>
+                            <li><span class="size">Double</span><span class="price">$3.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Americano</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">Single</span><span class="price">$2.50</span></li>
+                            <li><span class="size">Double</span><span class="price">$3.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Latte</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span><span class="price">$3.50</span></li>
+                            <li><span class="size">20oz</span><span class="price">$4.25</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc"></div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Flavored Latte</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span><span class="price">$4.25</span></li>
+                            <li><span class="size">20oz</span><span class="price">$5.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>
+                            Made with Monin cane sugar simple syrup. Ask about our flavor options!
+                        </p>
+                    </div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Mocha Latte</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span><span class="price">$4.25</span></li>
+                            <li><span class="size">20oz</span><span class="price">$5.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>
+                        Made with Hollander Chocolate Sauce. Choose between White or Dark
+                    Chocolate.
+                        </p>
+                    </div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Caramel Latte</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">12oz</span><span class="price">$4.25</span></li>
+                            <li><span class="size">20oz</span><span class="price">$5.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>                    
+                            Made with Hollander Salted Caramel sauce.
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div class="menu-smoothies">
+            <h1 class="menu_type">Smoothies</h1>
+            <ul class="menu_content">
+                <li class="product">
+                    <h3 class="product_title">Recovery</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">20oz</span><span class="price">$6.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>
+                        Strawberry, banana, whey or vegan protein, and amino repair blend
+                        </p>
+                    </div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">Berry Buzz</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">20oz</span><span class="price">$6.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>
+                        Blackberry, boysenberry, almonds, and energy blend of caffeine,
+                    taurine, and B vitamins
+                        </p>
+                    </div></li>
+ 	            <li class="product">
+                    <h3 class="product_title">Nutty Blueberry</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">20oz</span><span class="price">$6.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>
+                            Blueberry, banana, almonds, and an antioxidant blend
+                        </p>
+                    </div>
+                </li>
+ 	            <li class="product">
+                    <h3 class="product_title">BananaRama</h3>
+                    <div class="product_price">
+                        <ul>
+                            <li><span class="size">20oz</span><span class="price">$6.00</span></li>
+                        </ul>
+                    </div>
+                    <div class="product_desc">
+                        <p>
+                        Banana, peanuts, dark chocolate, and whey or vegan protein
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<?php get_footer() ?>
