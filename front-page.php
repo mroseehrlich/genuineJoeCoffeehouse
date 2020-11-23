c1da1ddf9cb5013fcf929314b6ce41ed153c3e3d
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2020-11-23 13:59:04 -0600

    add covid update

--- front-page.php
+++ front-page.php
@@ -8,6 +8,25 @@
  */
  get_header(); ?>
 
+ <article class="covid-info">
+     <div class="row">
+         <div class="col-md-6">
+             <h2>Patio Seating Now Open!</h2>
+             <p>
+                We can't wait to reopen fully, but for now, we're open for patio service only. Order coffee at the side door
+                and grab a seat on our front porch or on our newly shaded patio. All seating has been spaced to maintain
+                social distancing. Masks are required and dogs are welcome. Thank you for all your support through these tough
+                times. We wouldn't be here without you. We love you, Austin! 
+             </p>
+         </div>
+         <div class="hours">
+             <h2>Hours</h2>
+             <p>Monday-Friday 7AM-6PM</p>
+             <p>Saturday & Sunday 7AM - 3PM </p>
+         </div>
+     </div>
+ </article>
+
 <main>
     <div class="jumbotron jumbotron-fluid fade-in">
         <h1 class="brand-title">Genuine Joe Coffeehouse</h1>
