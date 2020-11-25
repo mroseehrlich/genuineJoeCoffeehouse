1b87f4f6822bb6e56bdbd1b228791453f22ef45c
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2020-11-25 16:45:21 -0600

    add contact section

--- front-page.php
+++ front-page.php
@@ -36,7 +36,7 @@
     </section>
     
     <section class="instagram-feed">
-        <h2 class="center">Instagram</h2>
+        <h2>Instagram</h2>
         <?php echo do_shortcode("[instagram-feed num=9]"); ?>
     </section>
 
@@ -46,22 +46,32 @@
                 <div class="cta-menu col-md-3">
                     <a class="cta-container__link" href="<?php site_url(); ?>/menu">
                         <h3>Menu</h3>
-                        <p>Check out our drinks menu </p>
+                        <p>Check out our drinks menu</p>
+                        <p><i class="fas fa-long-arrow-alt-right"></i></p>
                     </a>
                 </div>
                 <div class="cta-about col-md-3">
                     <a class="cta-container__link" href="<?php site_url(); ?>/about">
                         <h3>About Us</h3>
                         <p>Established 2006 in Austin's Crestview neighborhood</p>
+                        <p><i class="fas fa-long-arrow-alt-right"></i></p>
                     </a>
                 </div>
                 <div class="cta-contact col-md-3">
                     <a class="cta-container__link" href="<?php site_url(); ?>/contact">
                         <h3>Contact</h3>
                         <p>Questions? Concerns? Let us know</p>
+                        <p><i class="fas fa-long-arrow-alt-right"></i></p>
                     </a>
                 </div>
             </div>
+       </div>
+   </section>
+
+   <section class="contact">
+       <div class="container">
+       <h2>Visit Us</h2>
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3442.891976856561!2d-97.7295214847857!3d30.35401978177081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644cbb11d2292a5%3A0x25bb3fa206d43524!2s2001%20W%20Anderson%20Ln%2C%20Austin%2C%20TX%2078757!5e0!3m2!1sen!2sus!4v1605980819315!5m2!1sen!2sus" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
        </div>
    </section>
 
