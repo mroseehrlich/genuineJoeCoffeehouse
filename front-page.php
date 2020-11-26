5d4cc9a8c0f6c7428a7e45c3a39fe2177b659ef2
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2020-11-26 09:47:52 -0600

    update contact info text

--- front-page.php
+++ front-page.php
@@ -37,7 +37,12 @@
     
     <section class="instagram-feed">
         <h2>Instagram</h2>
-        <?php echo do_shortcode("[instagram-feed num=9]"); ?>
+        <div class="instagram-feed-container">
+            <div class="container">
+                <?php echo do_shortcode("[instagram-feed num=9]"); ?>
+            </div>
+          
+        </div>  
     </section>
 
    <section class="cta-container">
@@ -69,9 +74,24 @@
    </section>
 
    <section class="contact">
-       <div class="container">
        <h2>Visit Us</h2>
-       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3442.891976856561!2d-97.7295214847857!3d30.35401978177081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644cbb11d2292a5%3A0x25bb3fa206d43524!2s2001%20W%20Anderson%20Ln%2C%20Austin%2C%20TX%2078757!5e0!3m2!1sen!2sus!4v1605980819315!5m2!1sen!2sus" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+       <div class="contact-container">
+           <div class="container">
+               <div class="row">
+                 <div class="col-md-6">
+                    <iframe class="contact-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3442.891976856561!2d-97.7295214847857!3d30.35401978177081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644cbb11d2292a5%3A0x25bb3fa206d43524!2s2001%20W%20Anderson%20Ln%2C%20Austin%2C%20TX%2078757!5e0!3m2!1sen!2sus!4v1605980819315!5m2!1sen!2sus" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    
+                 </div>
+                 <div class="col-md-6">
+                     <h2>Genuine Joe Coffeehouse</h2>
+                     <div class="col-md-3">
+                     <p>2001 W Anderson Ln</p>
+                     <p>Austin, TX &nbsp; 78757</p>
+                     </div>
+                     
+                 </div>
+               </div>
+           </div>
        </div>
    </section>
 
