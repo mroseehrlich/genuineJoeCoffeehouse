8aecc78ccdffef011bd9dbe73aa7ba05d73a3672
Mia Ehrlich <m.rose.ehrlich@gmail.com>
2020-12-02 13:51:56 -0600

    change bootstrap columns to change below sm screen width

--- page-contact.php
+++ page-contact.php
@@ -10,16 +10,16 @@
 
 <div class="contact-info">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-sm-6">
             <h2>Contact</h2>
         </div>
             
-        <div class="col-md-6">
-            <a class="contact-info-link__phone">
+        <div class="contact-info__links col-sm-6">
+            <a class="contact-info-links__phone">
                 <span class="contact-phone__icon"><i class="fas fa-phone-square-alt"></i></span>
                 <span class="contact-phone__num">(512)202-1576</span>
             </a>
-            <a class="contact-info-link__email">
+            <a class="contact-info-links__email">
                 <span class="contact-email__icon"><i class="far fa-paper-plane"></i></span>
                 <span class="contact-email__address">info@genuinejoecoffee.com</span>
             </a>
@@ -31,11 +31,11 @@
 
 <div class="contact-address">
     <div class="row">
-        <div class="col-md-6">
+        <div class="contact-address__map col-sm-6">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3442.891976856561!2d-97.7295214847857!3d30.35401978177081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644cbb11d2292a5%3A0x25bb3fa206d43524!2s2001%20W%20Anderson%20Ln%2C%20Austin%2C%20TX%2078757!5e0!3m2!1sen!2sus!4v1605980819315!5m2!1sen!2sus" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         </div>
 
-        <div class="col-md-6">
+        <div class="contact-address__info col-sm-6">
             <h2>Visit</h2>
             <address>
                 2001 W Anderson Ln. <br/>
@@ -47,11 +47,11 @@
 
 <div class="contact-social">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-sm-6">
             <h2>Social</h2>
             <p>Follow us on social media and see what we're up to</p>
         </div>
-        <div class="col-md-6">
+        <div class="contact-social__links col-sm-6">
             <div class="contact-social__icons">
                 <i class="fab fa-facebook-f"></i><i class="fab fa-instagram"></i>
             </div>
